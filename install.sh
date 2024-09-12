c84fec98cbfcd190ea33b6287b733163b1fc0d92
Filounet <148258686+Filounet@users.noreply.github.com>
2024-09-12 11:48:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 mkdir -p ~/bin ~/cegep ~/projets
 
 sudo pacman -Syu
-sudo pacman -S --nocomfirm git vim gcc python3 python3-pip python3-venv
+sudo pacman -Sy  git vim gcc python3 python3-pip python3-venv
 
 cp .gitconfig ~/
 cp .bashrc ~/
